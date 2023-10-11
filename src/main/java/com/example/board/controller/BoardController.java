20e03c6514f10e78dd17d56b92110b9b2af7b427
LeeSeungHun <seung29954@gmail.com>
2023-10-11 23:22:30 +0900

    logback > controller에 Slf4j 사용 (어노테이션 및 생성)

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -2,6 +2,9 @@ package com.example.board.controller;
 
 import com.example.board.dto.BoardDto;
 import com.example.board.service.BoardService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +14,19 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
+@Slf4j
 public class BoardController {
 
     @Autowired
     private BoardService boardService;
 
+    // @Slf4j 사용하면 로거 생성 필요없음.
+//     private Logger log = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/board/openBoardList.do")
     public ModelAndView openBoardList() throws Exception {
+        log.debug("BoardController :: openBoardList");
+
         ModelAndView mv = new ModelAndView("/board/boardList");
 
         List<BoardDto> list = boardService.selectBoardList();
@@ -28,17 +37,23 @@ public class BoardController {
 
     @RequestMapping("/board/openBoardWrite.do")
     public String openBoardWrite() throws Exception{
+        log.debug("BoardController :: openBoardWrite");
+
         return "/board/boardWrite";
     }
 
     @RequestMapping("/board/insertBoard.do")
     public String insertBoard(BoardDto dto) throws Exception{
+        log.debug("BoardController :: insertBoard");
+
         boardService.insertBoard(dto);
         return "redirect:/board/openBoardList.do";
     }
 
     @RequestMapping("/board/openBoardDetail.do")
     public ModelAndView openBoardDetail(@RequestParam int boardIdx) throws Exception{
+        log.debug("BoardController :: openBoardDetail");
+
         ModelAndView mv = new ModelAndView("/board/boardDetail");
         BoardDto board = boardService.selectBoardDetail(boardIdx);
         mv.addObject("board", board);
@@ -48,12 +63,16 @@ public class BoardController {
 
     @RequestMapping("/board/updateBoard.do")
     public String updateBoard(BoardDto dto) throws Exception{
+        log.debug("BoardController :: updateBoard");
+
         boardService.updateBoard(dto);
         return "redirect:/board/openBoardList.do";
     }
 
     @RequestMapping("/board/deleteBoard.do")
     public String deleteBoard(int boardIdx) throws Exception{
+        log.debug("BoardController :: deleteBoard");
+
         boardService.deleteBoard(boardIdx);
         return "redirect:/board/openBoardList.do";
     }
