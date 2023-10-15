e45d3a580a5991da169b3a4c5faa2db667c6388b
LeeSeungHun <fjqmdls22@naver.com>
2023-10-15 21:34:48 +0900

    파일 업로드하기 - 설정 1. 파일 라이브러리 추가 (아파치 Common Fileupload) 2. 파일처리를 위한 빈 설정 (WebMvcConfiguraion) 3. 파일 관련 자동구성 제거(BoardApplication)

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,8 +2,10 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
-@SpringBootApplication
+// 스프링부트에서 자동으로 구성된 첨부파일 관련 MultipartAutoConfiguration 클래스 제외
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 public class BoardApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/board/configuration/WebMvcConfiguration.java
+++ src/main/java/com/example/board/configuration/WebMvcConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.board.configuration;
 
 import com.example.board.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggerInterceptor());
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩
+        commonsMultipartResolver.setMaxUploadSizePerFile(5 *1024 * 1024); // 5mb (바이트 단위로 업로드 되는 파일 크기)
+        return commonsMultipartResolver;
     }
 }
