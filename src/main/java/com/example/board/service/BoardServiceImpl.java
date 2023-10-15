240a98528857ce6ef7753897502d1e9075ee5c22
LeeSeungHun <fjqmdls22@naver.com>
2023-10-15 16:41:55 +0900

    service 에 transaction 추가

--- src/main/java/com/example/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/board/service/BoardServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.board.dto.BoardDto;
 import com.example.board.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,27 +15,33 @@ public class BoardServiceImpl implements BoardService {
     private BoardMapper boardMapper;
 
     @Override
+    @Transactional(readOnly = true)
     public List<BoardDto> selectBoardList() throws Exception{
         return boardMapper.selectBoardList();
     }
 
     @Override
+    @Transactional
     public void insertBoard(BoardDto dto) throws Exception{
         boardMapper.insertBoard(dto);
     }
 
     @Override
+    @Transactional
     public BoardDto selectBoardDetail(int boardIdx) throws Exception{
         boardMapper.updateHitCount(boardIdx);
+
         return boardMapper.selectBoardDetail(boardIdx);
     }
 
     @Override
+    @Transactional
     public void updateBoard(BoardDto dto) throws Exception{
         boardMapper.updateBoard(dto);
     }
 
     @Override
+    @Transactional
     public void deleteBoard(int boardIdx) throws Exception{
         boardMapper.deleteBoard(boardIdx);
     }
