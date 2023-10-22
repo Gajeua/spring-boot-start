e316b96c764b58d054350dd9c6796079574302d1
LeeSeungHun <fjqmdls22@naver.com>
2023-10-22 21:50:08 +0900

    swagger 적용

--- src/main/java/com/example/board/controller/RestBoardApiController.java
+++ src/main/java/com/example/board/controller/RestBoardApiController.java
@@ -2,6 +2,7 @@ package com.example.board.controller;
 
 import com.example.board.dto.BoardDto;
 import com.example.board.service.BoardService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "게시판 REST API")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -17,29 +19,34 @@ public class RestBoardApiController {
     @Autowired
     private final BoardService boardService;
 
+    @ApiOperation(value = "게시글 목록 조회", notes = "게시글 목록을 조회합니다.")
     @GetMapping("/api/board")
     public List<BoardDto> openBoardList() throws Exception {
         return boardService.selectBoardList();
     }
 
+    @ApiOperation(value = "게시글 작성", notes = "게시글을 작성니다.")
     @PostMapping("/api/board/write")
     public void insertBoard(@RequestBody BoardDto boardDto) throws Exception {
         boardService.insertBoard(boardDto, null);
     }
 
+    @ApiOperation(value = "게시글 상세 조회", notes = "게시글을 상세 조회합니다.")
     @GetMapping("/api/board/{boardIdx}")
-    public BoardDto openBoardDetail(@PathVariable int boardIdx) throws Exception{
+    public BoardDto openBoardDetail(@PathVariable @ApiParam(value = "게시글 번호") int boardIdx) throws Exception{
         return boardService.selectBoardDetail(boardIdx);
     }
 
+    @ApiOperation(value = "게시글 수정", notes = "게시글을 수정합니다.")
     @PutMapping("/api/board/{boardIdx}")
     public String updateBoard(@PathVariable int boardIdx, @RequestBody BoardDto dto) throws Exception{
         boardService.updateBoard(dto);
         return "redirect:/board";
     }
 
+    @ApiOperation(value = "게시글 삭제", notes = "게시글을 삭제합니다.")
     @DeleteMapping("/api/board/{boardIdx}")
-    public String deleteBoard(@PathVariable int boardIdx) throws Exception{
+    public String deleteBoard(@PathVariable @ApiParam(value = "게시글 번호") int boardIdx) throws Exception{
         boardService.deleteBoard(boardIdx);
         return "redirect:/board";
     }
--- src/main/java/com/example/board/dto/BoardDto.java
+++ src/main/java/com/example/board/dto/BoardDto.java
@@ -1,19 +1,39 @@
 package com.example.board.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
+@ApiModel(value = "BoardDto : 게시글 내용", description = "게시글 내용")
 @Data
 public class BoardDto {
+    
+    @ApiModelProperty(value = "게시글 번호")
     private int boardIdx;
+
+    @ApiModelProperty(value = "제목")
     private String title;
+
+    @ApiModelProperty(value = "내용")
     private String contents;
+
+    @ApiModelProperty(value = "조회수")
     private int hitCnt;
+
+    @ApiModelProperty(value = "작성자")
     private String creatorId;
+
+    @ApiModelProperty(value = "작성시간")
     private String createdDatetime;
+
+    @ApiModelProperty(value = "수정자")
     private String updaterId;
+
+    @ApiModelProperty(value = "수정시간")
     private String updatedDatetime;
 
+    @ApiModelProperty(value = "첨부파일 목록")
     private List<BoardFileDto> boardFileDtoList;
 }
